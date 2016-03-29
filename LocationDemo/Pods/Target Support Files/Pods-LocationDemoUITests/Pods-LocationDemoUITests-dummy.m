126b6f7609d15a0e7447f0cbf0f08278163fb7bf
Teng <843018739@qq.com>
2016-03-29 20:27:30 +0800

    init LocationDemo project

---
+++ LocationDemo/Pods/Target Support Files/Pods-LocationDemoUITests/Pods-LocationDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocationDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LocationDemoUITests
+@end
---
+++ LocationDemo/Pods/Target Support Files/Pods-LocationDemoUITests/Pods-LocationDemoUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LocationDemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LocationDemoUITestsVersionString[];
+
