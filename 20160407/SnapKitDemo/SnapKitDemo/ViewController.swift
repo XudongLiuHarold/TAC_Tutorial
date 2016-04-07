edb10cc3524c3d188b4f3be1960205ac77929a8c
anzhehong <anzhehong@126.com>
2016-04-07 16:16:36 +0800

    update snapkit demo

--- 20160407/SnapKitDemo/SnapKitDemo/ViewController.swift
+++ 20160407/SnapKitDemo/SnapKitDemo/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - ViewController Life Circle
     override func viewDidLoad() {
         super.viewDidLoad()
+//        original()
 //        addLabel()
 //        addBox()
 //        testEdges()
 //        testSize()
 //        testCenter()
-//        testReadble()
-//        original()
+        testReadble()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,86 +30,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func addLabel() {
-        let label = UILabel()
-        label.text = "Let's play with SnapKit"
-        label.textAlignment = .Center
-        label.font = UIFont.boldSystemFontOfSize(20)
-        label.textColor = UIColor.CustomGreenColor()
-        self.view.addSubview(label)
-        label.snp_makeConstraints { (make) in
-            make.leading.equalTo(self.view.snp_leading)
-            make.trailing.equalTo(self.view.snp_trailing)
-            make.centerY.equalTo(self.view)
-        }
-    }
-    
-    func addBox() {
-        let box = UIView()
-        box.backgroundColor = UIColor.CustomGreenColor()
-        let superView = self.view
-        superView.addSubview(box)
-        
-        /**
-         *  Method One
-         */
-        box.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(superView).offset(20)
-            make.left.equalTo(superView).offset(20)
-            make.bottom.equalTo(superView).offset(-20)
-            make.right.equalTo(superView).offset(-20)
-        }
-        
-        /**
-         *  Method Two
-         */
-        box.snp_makeConstraints { (make) -> Void in
-            make.edges.equalTo(superView).inset(UIEdgeInsetsMake(20, 20, 20, 20))
-        }
-    }
-    
-    func testEdges() {
-        let view = UIView()
-        view.backgroundColor = UIColor.CustomGreenColor()
-        let superView = self.view
-        superView.addSubview(view)
-        view.snp_makeConstraints { (make) in
-            make.edges.equalTo(superView).inset(UIEdgeInsetsMake(5, 10, 20, 50))
-        }
-    }
-    
-    func testSize() {
-        let view = UIView()
-        view.backgroundColor = UIColor.CustomGreenColor()
-        let superView = self.view
-        superView.addSubview(view)
-        view.snp_makeConstraints { (make) in
-            make.size.equalTo(superView).offset(CGSizeMake(-100, -50))
-        }
-    }
-    
-    func testCenter() {
-        let view = UIView()
-        view.backgroundColor = UIColor.CustomGreenColor()
-        let superView = self.view
-        superView.addSubview(view)
-        view.snp_makeConstraints { (make) in
-            make.center.equalTo(superView).offset(CGPointMake(-30, 20))
-            make.size.equalTo(superView).offset(CGSizeMake(-200, -200))
-        }
-    }
-    
-    func testReadble() {
-        let view = UIView()
-        view.backgroundColor = UIColor.CustomGreenColor()
-        let superView = self.view
-        superView.addSubview(view)
-        view.snp_makeConstraints { (make) in
-            make.left.right.bottom.equalTo(superView)
-            make.top.equalTo(superView).offset(100)
-        }
-    }
-    
+    //MARK: - NSLayoutConstraint布局
+    /**
+     底部均与分布的两个色块
+     */
     func original() {
         
         let blueView = UIView()
@@ -133,7 +58,7 @@ class ViewController: UIViewController {
                                             toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
         
         let equalWidth = NSLayoutConstraint(item: blueView, attribute: .Width, relatedBy: .Equal,
-                                           toItem: redView, attribute: .Width, multiplier: 1.0, constant: 0)
+                                            toItem: redView, attribute: .Width, multiplier: 1.0, constant: 0)
         
         let redTop = NSLayoutConstraint(item: redView, attribute: .Top, relatedBy: .Equal,
                                         toItem: blueView, attribute: .Top, multiplier: 1.0, constant: 0)
@@ -146,9 +71,111 @@ class ViewController: UIViewController {
         
         self.view.addConstraints([blueLeft, blueBottom, blueRight, blueHeight, equalWidth, redTop, redBottom, redRight])
     }
-
+    
+    //MARK: - SnapKit方法布局
+    /**
+     屏幕中间的label
+     使用leading,trailing,center
+     */
+    func addLabel() {
+        let label = UILabel()
+        label.text = "Let's play with SnapKit"
+        label.textAlignment = .Center
+        label.font = UIFont.boldSystemFontOfSize(20)
+        label.textColor = UIColor.CustomGreenColor()
+        self.view.addSubview(label)
+        label.snp_makeConstraints { (make) in
+            make.leading.equalTo(self.view.snp_leading)
+            make.trailing.equalTo(self.view.snp_trailing)
+            make.centerY.equalTo(self.view)
+        }
+    }
+    
+    /**
+     两种方法在屏幕放置一个和屏幕几乎等大的色块
+     分别使用top,bottom,left,right
+     */
+    func addBox() {
+        let box = UIView()
+        box.backgroundColor = UIColor.CustomGreenColor()
+        let superView = self.view
+        superView.addSubview(box)
+        
+        /**
+         *  Method One
+         */
+        box.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(superView).offset(20)
+            make.left.equalTo(superView).offset(20)
+            make.bottom.equalTo(superView).offset(-20)
+            make.right.equalTo(superView).offset(-20)
+        }
+        
+        /**
+         *  Method Two
+         */
+//        box.snp_makeConstraints { (make) -> Void in
+//            make.edges.equalTo(superView).inset(UIEdgeInsetsMake(20, 20, 20, 20))
+//        }
+    }
+    
+    /**
+     效果和上面一个类似
+     使用edges属性
+     */
+    func testEdges() {
+        let view = UIView()
+        view.backgroundColor = UIColor.CustomGreenColor()
+        let superView = self.view
+        superView.addSubview(view)
+        view.snp_makeConstraints { (make) in
+            make.edges.equalTo(superView).inset(UIEdgeInsetsMake(5, 10, 20, 50))
+        }
+    }
+    
+    /**
+     使用size属性
+     */
+    func testSize() {
+        let view = UIView()
+        view.backgroundColor = UIColor.CustomGreenColor()
+        let superView = self.view
+        superView.addSubview(view)
+        view.snp_makeConstraints { (make) in
+            make.size.equalTo(superView).offset(CGSizeMake(-100, -50))
+        }
+    }
+    
+    /**
+     使用center属性
+     */
+    func testCenter() {
+        let view = UIView()
+        view.backgroundColor = UIColor.CustomGreenColor()
+        let superView = self.view
+        superView.addSubview(view)
+        view.snp_makeConstraints { (make) in
+            make.center.equalTo(superView).offset(CGPointMake(-30, 20))
+            make.size.equalTo(superView).offset(CGSizeMake(-200, -200))
+        }
+    }
+    
+    /**
+     更为可读、简易的方式
+     */
+    func testReadble() {
+        let view = UIView()
+        view.backgroundColor = UIColor.CustomGreenColor()
+        let superView = self.view
+        superView.addSubview(view)
+        view.snp_makeConstraints { (make) in
+            make.left.right.bottom.equalTo(superView)
+            make.top.equalTo(superView).offset(100)
+        }
+    }
 }
 
+//MARK: - Custom UIColor
 extension UIColor {
     class func CustomGreenColor() -> UIColor {
         return UIColor(red: 36/255, green: 169/255, blue: 255/255, alpha: 1.0)
